40dc15731d5cb033d0f5a299235932d6cf29658e
Dan Vera <dan@ianalyst.com>
2013-11-20 17:32:55 -0500

    df

--- ticker.php
+++ ticker.php
@@ -5,7 +5,7 @@ header("Content-type: text/json");
 
 include($_SERVER['DOCUMENT_ROOT'] .'/config/database.php');
 
-$result = $conn->query('SELECT * FROM USD ORDER BY id DESC LIMIT 1');
+$result = $conn->query('SELECT * FROM USD ORDER BY id DESC');
 foreach($result as $row){
     $ret = intval( $row['r_timestamp_unix']);
     $ret2= intval( $row['ask']);
